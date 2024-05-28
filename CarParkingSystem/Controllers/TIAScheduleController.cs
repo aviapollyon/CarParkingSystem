5dfe418c68c2e0b567d17b203b03d88efd9eba4c
aviapollyon <aviapollyon@gmail.com>
2024-05-28 17:51:06 +0200

    wtwt

---
+++ CarParkingSystem/Controllers/TIAScheduleController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarParkingSystem.Controllers
+{
+    public class TIAScheduleController : Controller
+    {
+        public IActionResult admin()
+        {
+            return View();
+        }
+        public IActionResult guard()
+        {
+            return View();
+        }
+    }
+}
